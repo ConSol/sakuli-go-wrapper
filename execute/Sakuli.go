6e182d0d043ac2ada8d095f2595a33f3d99e55e5
toschneck <tobias.schneck@consol.de>
2016-02-19 11:36:25 +0100

    print  go error message only if sakuli return code is not between 0 and 6

--- execute/Sakuli.go
+++ execute/Sakuli.go
@@ -14,7 +14,8 @@ func RunSakuli(javaExecutable, sakuliJars string, javaProperties, sakuliProperti
 	classpath := helper.GenClassPath(filepath.Join(sakuliJars, "sakuli.jar"), filepath.Join(sakuliJars, "*"))
 	args := []string{}
 	args = append(args, javaProperties...)
-	args = append(args, "-Duser.dir=" + os.Getenv("SAKULI_HOME") )
+	//TODO:if -sakuli home is set use this value
+	args = append(args, "-Duser.dir=" + os.Getenv("SAKULI_HOME"))
 	args = append(args, "-classpath")
 	args = append(args, classpath)
 	args = append(args, "org.sakuli.starter.SakuliStarter")
@@ -23,9 +24,11 @@ func RunSakuli(javaExecutable, sakuliJars string, javaProperties, sakuliProperti
 	fmt.Print(javaExecutable, " ", strings.Join(args, " "))
 	fmt.Println(" ===========")
 	returnCode, err := Execute(javaExecutable, args...)
-	if err != nil {
-		fmt.Println("Error while calling Sakuli JAR")
+	if returnCode < 0 || returnCode > 6 {
+		if err != nil {
+			fmt.Println("Error while calling Sakuli JAR:\n" + err.Error())
+		}
+		fmt.Printf("=========== Sakuli JAR finished with returncode: %d ===========\n", returnCode)
 	}
-	fmt.Printf("=========== Sakuli JAR finished with returncode: %d ===========\n", returnCode)
 	return returnCode
 }
